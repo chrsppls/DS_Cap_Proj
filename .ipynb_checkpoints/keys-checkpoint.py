17f7e562ad3344fcd34f5efa0f64d9ccfbc06b5d
chrs.ppls <chrs_ppls@yahoo.com>
2020-05-05 10:27:23 -0400

    first commit

---
+++ .ipynb_checkpoints/keys-checkpoint.py
@@ -0,0 +1,10 @@
+# Map
+map_token = 'pk.eyJ1IjoiY2hyc3BwbHMiLCJhIjoiY2s5YnlwdTI2MmpubjNqbXl3YmE3OHM1ZyJ9.cZZd_KrU4xZwiHmt-YwCKQ'
+
+# Foursquare credentials
+CLIENT_ID = 'I5IIUFHTNLOAEZCYFUC1LS5MJ2XYPTBCQKCRPKUVVI2YVVIN' # your Foursquare ID
+CLIENT_SECRET = 'AKHAQHZJ51MNDCJQXAQJT1SOL1OEBF4WDZD5A0KOGCUXZBQW' # your Foursquare Secret
+
+# Maryland Data
+MD_ID = 'e9a5mnwrm7lo94900mlvx4jpd'
+MD_PW = '1bvl51zp0op4iw1qroqjvmwbmb59ud81repzr8mrd07detpjvd'
---
+++ README.md
@@ -0,0 +1,2 @@
+# Data_Science_CP_Proj
+# DS_Cap_Proj
